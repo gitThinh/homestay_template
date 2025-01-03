22d72acb00a2f1359c5098e9dd8d8d190d7691d8
gitThinh <nghthinh1998@gmail.com>
2025-01-03 22:19:41 +0700

    base setup

--- src/layouts/sidebarLayout.vue
+++ src/layouts/sidebarLayout.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="grid grid-cols-12 gap-5">
-    <div class="col-span-12 lg:col-span-8">
+    <div :class="isLeft ? 'order-2 col-span-12 lg:col-span-8' : 'col-span-12 lg:col-span-8'">
       <slot name="content"></slot>
     </div>
-    <div class="hidden lg:block col-span-4 sticky top-14 h-max">
+    <div :class="isLeft ? 'order-1 hidden lg:block col-span-4 sticky top-14 h-max' : 'hidden lg:block col-span-4 sticky top-14 h-max'">
       <slot name="sidebar"></slot>
     </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+defineProps({
+  isLeft: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
---
+++ src/pages/search/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtLayout name="white-layout">
+    <div class="header bg_beach_image object-contain w-full aspect-video lg:aspect-[5/2] lg:mx-20 lg:rounded-xl lg:w-auto">
+        <div class=""></div>
+    </div>
+    <div class="main_container">
+    </div>
+  </NuxtLayout>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped>
+</style>
